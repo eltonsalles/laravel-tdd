82522cd7d49732ba53524e4cc53f3ad99f76a50c
Elton Rodrigues <eltonrms.leite@hotmail.com>
2018-03-23 11:04:11 -0300

    preparando os controllers da api

---
+++ app/Http/Controllers/Api/AccountsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Account;
+use App\Http\Controllers\ApiControllerTrait;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class AccountsController extends Controller
+{
+    use ApiControllerTrait;
+
+    protected $model;
+    protected $relationships = ['bank'];
+
+    public function __construct(Account $model)
+    {
+        $this->model = $model;
+    }
+}
